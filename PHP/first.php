e2b4515b341fcf98fdc2637a3b7ec5bb4d24f882
Cromwell Bost <cbost@emich.edu>
2025-02-16 03:33:25 -0500

    initial commit

---
+++ PHP/first.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+<body>
+
+    <!-- PHP Script is below -->
+
+
+     <?php
+
+    $first_name = 'Cromwell';
+    $last_name = 'Bost';
+
+     print 'Hello! My name is $first_name $last_name';
+
+     ?>
+
+</body>
+</html> 
